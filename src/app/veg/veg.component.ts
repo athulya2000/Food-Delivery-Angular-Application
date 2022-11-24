1e52c0201607f6fbaf70382508ed298f07331737
athulya2000 <athulyav00@gmail.com>
2022-11-24 23:01:04 +0530

    veg completed

---
+++ src/app/veg/veg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-veg',
+  templateUrl: './veg.component.html',
+  styleUrls: ['./veg.component.css']
+})
+export class VegComponent {
+
+}
